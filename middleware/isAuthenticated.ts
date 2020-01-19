fb730b76cd53f74edaea99aefff0be7aa362c504
marwanamr3 <marwan.abdelgawad@student.guc.edu.eg>
2020-01-19 06:12:48 +0200

    add/remove favorite

--- middleware/isAuthenticated.ts
+++ middleware/isAuthenticated.ts
@@ -1,19 +1,22 @@
-import {IMiddleware} from "../Interfaces/IMiddleware";
+import { IMiddleware } from "../Interfaces/IMiddleware";
 import UnauthorizedException from "../exception/UnauthorizedException";
-import {promises as fs} from "fs";
+import { promises as fs } from "fs";
 import path from "path";
 import jwt from "jsonwebtoken";
 
-class IsAuthenticated implements IMiddleware{
+class IsAuthenticated implements IMiddleware {
 
     async handle(request: any, response: any, next: any): Promise<any> {
-        let token = request.headers['authorization'].replace('Bearer','').trim();
+        let token = request.headers['authorization'];
+        if (!token)
+            throw new UnauthorizedException('No token found, please login');
+        token = request.headers['authorization'].replace('Bearer', '').trim();
         try {
             let publicKey = await fs.readFile(path.join(__dirname, '../keys/jwtRS256.key.pub'));
             let decoded = await jwt.verify(token, publicKey);
             request.user = decoded;
             next();
-        }catch (error) {
+        } catch (error) {
             throw new UnauthorizedException('Wrong Credentials');
         }
     }
