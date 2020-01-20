6b92f2316b3e3e717d1237894a1fdfe99c00ecc7
youssefnabil48 <youssefnabil48@outlook.com>
2020-01-20 04:07:18 +0200

    fix middleware exception throwing

--- middleware/isInvestor.ts
+++ middleware/isInvestor.ts
@@ -1,10 +1,14 @@
 import { IMiddleware } from "../Interfaces/IMiddleware";
-
+import UnauthorizedException from "../exception/UnauthorizedException";
 
 class IsInvestor implements IMiddleware{
 
     async handle(request: any, response: any, next: any): Promise<any> {
-
+        let user = request.user;
+        if(user.userType !== 'investor'){
+            throw new UnauthorizedException('User must be a client', response);
+        }
+        next();
     }
 }
 
