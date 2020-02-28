0dcf7635d720e30e6dfcee04d5af1980a00123cc
youssefnabil48 <youssefnabil48@outlook.com>
2020-02-28 18:02:26 +0200

    client seeds

--- seed.ts
+++ seed.ts
@@ -7,12 +7,23 @@ import investorRepo from "./api/Investor/InvestorRepository";
 import propertiesRepo from "./api/Property/PropertyRepository";
 import { startDB, stopDB } from "./database";
 import { UserType, PropType } from "./enums";
-import { IInvestor, IProperty } from "./database/models";
+import { IInvestor, IProperty, IClient, IRentBuyRequest, IAddPropertyRequest, IContract, Contract, RentBuyRequest } from "./database/models";
 import faker from "./lib/faker";
+import AddPropertyRequestRepository from "./api/Request/AddPropertyRequest/AddPropertyRequestRepository";
+import RentBuyRequestRepository from "./api/Request/RentBuyRequest/RentBuyRequestRepository";
 
 let investors: IInvestor[];
+let clients: IClient[];
+let properties: IProperty[] = [];
 let investorOneProperties: IProperty[];
 let investorTwoProperties: IProperty[];
+let investorThreeProperties: IProperty[];
+let investorFourProperties: IProperty[];
+let rentRequests: IRentBuyRequest[];
+let buyRequest: IRentBuyRequest[];
+let addPropertyRequests: IAddPropertyRequest[];
+let contracts: IContract[];
+
 
 async function seedAdmin(): Promise<void> {
   const password = await bcrypt.hash("123123123", 10);
@@ -47,6 +58,26 @@ async function seedInvestors(): Promise<void> {
       phoneNumber: "123456788",
       gender: "male",
       isVerified: true
+    }),
+    investorRepo.create({
+      password,
+      userType: UserType.Investor,
+      firstName: "Investor",
+      lastName: "three",
+      email: "investorthree@sakan.me",
+      phoneNumber: "123456789",
+      gender: "male",
+      isVerified: true
+    }),
+    investorRepo.create({
+      password,
+      userType: UserType.Investor,
+      firstName: "Investor",
+      lastName: "Four",
+      email: "investorfour@sakan.me",
+      phoneNumber: "123456788",
+      gender: "male",
+      isVerified: true
     })
   ]);
   console.log("seeded Investors");
@@ -66,21 +97,36 @@ async function seedProperty(owner: string): Promise<any> {
     bathroomNum: faker.random.number(10),
     unitArea: faker.random.number(3000),
     rentValue: faker.random.number(10000),
+    buyValue: faker.random.number(10000000),
     isApproved: true
   });
 }
 
 async function seedProperties() {
   investorOneProperties = await Promise.all(
-    Array(10)
+    Array(5)
       .fill(null)
       .map(() => seedProperty(investors[0].id))
   );
+  properties = properties.concat(investorOneProperties);
   investorTwoProperties = await Promise.all(
-    Array(10)
+    Array(5)
       .fill(null)
       .map(() => seedProperty(investors[1].id))
   );
+  properties = properties.concat(investorTwoProperties);
+  investorThreeProperties = await Promise.all(
+    Array(5)
+      .fill(null)
+      .map(() => seedProperty(investors[2].id))
+  );
+  properties = properties.concat(investorThreeProperties);
+  investorFourProperties = await Promise.all(
+    Array(5)
+      .fill(null)
+      .map(() => seedProperty(investors[3].id))
+  );
+  properties = properties.concat(investorFourProperties);
   console.log("Seeded Properties");
 }
 
@@ -101,13 +147,200 @@ async function updateOwners(): Promise<void> {
       })
     )
   );
+  await Promise.all(
+    investorThreeProperties.map(p =>
+      investorRepo.setUpdateById(investors[2].id, {
+        $addToSet: { ownedProps: p.id }
+      })
+    )
+  );
+  console.log("Updated Investor One");
+  await Promise.all(
+    investorFourProperties.map(p =>
+      investorRepo.setUpdateById(investors[3].id, {
+        $addToSet: { ownedProps: p.id }
+      })
+    )
+  );
   console.log("Updated Investor Two");
 }
 
+async function seedClients(): Promise<void> {
+  const password = await bcrypt.hash("123123123", 10);
+  clients = await Promise.all([
+    investorRepo.create({
+      password,
+      wallet: {
+        value: 998,
+        currency: "EGP"
+      },
+      userType: UserType.Client,
+      firstName: "client",
+      lastName: "One",
+      email: "clientone@sakan.me",
+      phoneNumber: "123456756",
+      gender: "male",
+      isVerified: true
+    }),
+    investorRepo.create({
+      password,
+      wallet: {
+        value: 13123,
+        currency: "EGP"
+      },
+      userType: UserType.Client,
+      firstName: "client",
+      lastName: "Two",
+      email: "clienttwo@sakan.me",
+      phoneNumber: "123456745",
+      gender: "male",
+      isVerified: true
+    }),
+    investorRepo.create({
+      password,
+      wallet: {
+        value: 10008899,
+        currency: "EGP"
+      },
+      userType: UserType.Client,
+      firstName: "client",
+      lastName: "Three",
+      email: "clientthree@sakan.me",
+      phoneNumber: "1234567445",
+      gender: "male",
+      isVerified: true
+    }),
+    investorRepo.create({
+      password,
+      wallet: {
+        value: 100000000,
+        currency: "EGP"
+      },
+      userType: UserType.Client,
+      firstName: "client",
+      lastName: "Four",
+      email: "clientfour@sakan.me",
+      phoneNumber: "1234567491",
+      gender: "male",
+      isVerified: true
+    })
+  ]);
+  console.log("seeded Clients");
+}
+
+async function seedAddPropertyRequest(owner: string): Promise<any> {
+  return AddPropertyRequestRepository.create({
+    owner,
+    propType: faker.helpers.randomize(Object.values(PropType)),
+    address: {
+      street: faker.address.streetAddress(),
+      city: faker.address.city(),
+      country: "Egypt"
+    },
+    description: faker.lorem.paragraph(),
+    bedroomNum: faker.random.number(10),
+    bathroomNum: faker.random.number(10),
+    unitArea: faker.random.number(3000),
+    rentValue: faker.random.number(10000),
+    buyValue: faker.random.number(10000000),
+    isApproved: false,
+    status: "pending approval"
+  });
+}
+
+async function seedAddRequests() {
+  addPropertyRequests = await Promise.all(
+    Array(4)
+      .fill(null)
+      .map((value, index) => seedAddPropertyRequest(investors[index].id))
+  );
+  console.log('Seeded Add Property Requests')
+}
+
+async function invoices(property: IProperty) {
+  // let startingDate = new Date(faker.date.future());
+  let paidInvoices = Array(faker.random.number(10)).fill(null).map((value, index) => {
+    return {
+      invoiceNumber: index + 1,
+      // dueDate: startingDate.setMonth(startingDate.getMonth() + index).toString(),
+      dueDate: faker.date.future(),
+      isPaid: true,
+      value: property.rentValue,
+      penaltyValue: 0
+    }
+  });
+  paidInvoices.push({
+    invoiceNumber: paidInvoices.length + 1,
+    dueDate: faker.date.future(),
+    isPaid: true,
+    value: property.rentValue,
+    penaltyValue: 0
+  });
+  return paidInvoices;
+}
+
+async function seedContract(
+  property: IProperty,
+  client: IClient
+): Promise<any> {
+  let contractInvoices = await invoices(property);
+  return Contract.create({
+    contractType: 'rent',
+    status: 'active',
+    propertyId: property.id,
+    ownerId: property.owner,
+    clientId: client.id,
+    contractInvoices
+  });
+}
+
+async function seedContracts(): Promise<any> {
+  await Promise.all(
+    Array(4)
+      .fill(null)
+      .map((value, index) =>
+        seedContract(properties[index], clients[index])
+      )
+  )
+  console.log('Seeded Contracts');
+}
+
+
+async function seedRentRequest(
+  clientId: string,
+  property: IProperty
+): Promise<any> {
+  return RentBuyRequestRepository.create({
+    reqType: 'rent',
+    ownerId: property.owner,
+    clientId: clientId,
+    propertyId: property.id,
+    isApproved: false,
+    status: 'pending approval'
+  });
+}
+
+async function seedRentRequests() {
+  rentRequests = await Promise.all(
+    Array(4)
+      .fill(null)
+      .map((value, index) => seedRentRequest(clients[index].id, properties[index]))
+  );
+  console.log('Seeded Rent Requests')
+}
+
 (async function seed() {
-  await startDB();
-  await Promise.all([seedAdmin(), seedInvestors()]);
-  await seedProperties();
-  await updateOwners();
-  await stopDB();
+  try {
+    await startDB();
+    await Promise.all([seedAdmin(), seedInvestors()]);
+    await seedProperties();
+    await updateOwners();
+    await seedClients();
+    await seedAddRequests();
+    await seedContracts();
+    await seedRentRequests();
+    await stopDB();
+  } catch (error) {
+    console.log(error);
+  }
 })();
