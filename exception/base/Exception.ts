1f203964cb8e7bcaa33ab37041162fcb69424397
youssefnabil48 <youssefnabil48@outlook.com>
2020-03-08 23:27:51 +0200

    fixed exceptions status code bug

--- exception/base/Exception.ts
+++ exception/base/Exception.ts
@@ -1,8 +1,10 @@
 import { Response } from "express";
 
 export default class Exception extends Error{
+    statusCode: Number;
     constructor(message: string,statusCode: number , response?: Response){
         super(message);
+        this.statusCode = statusCode;
         if(response) response.status(statusCode).send({
             data: null,
             message: message || super.message
