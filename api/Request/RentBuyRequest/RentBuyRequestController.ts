7d2c081fd6cdfc9bb430976a7aa65d6a3f401c0c
Abdelrahman Soliman <bebofit@aucegypt.edu>
2020-06-14 20:38:32 +0200

    heroku initial

--- api/Request/RentBuyRequest/RentBuyRequestController.ts
+++ api/Request/RentBuyRequest/RentBuyRequestController.ts
@@ -9,16 +9,21 @@ import Messages = require('../../Constants/Messages');
 import ConflictException from '../../../exception/ConflictException';
 import Http from '../../Utils/Http';
 
-class RentBuyRequestController{
-  constructor() {
-  }
+class RentBuyRequestController {
+  constructor() {}
 
-  async createRentBuyRequest(request: IRequest, response: Response): Promise<any> {
+  async createRentBuyRequest(
+    request: IRequest,
+    response: Response
+  ): Promise<any> {
     let rentBuyReq;
     try {
       //validating json object
-      const body = validation.validateBody(request.body, rentbuyReqValidations.CREATE);
-      Object.assign(body, {clientId: request.user.id});
+      const body = validation.validateBody(
+        request.body,
+        rentbuyReqValidations.CREATE
+      );
+      Object.assign(body, { clientId: request.user.id });
       //creating the new add request
       rentBuyReq = await rentbuyReqService.createRequest(body);
     } catch (error) {
@@ -28,35 +33,72 @@ class RentBuyRequestController{
       throw error;
     }
     //sending response
-    return Http.sendResponse(response, httpStatus.CREATED, rentBuyReq, "Rent/Buy Property Request Created Successfully");
+    return Http.sendResponse(
+      response,
+      httpStatus.CREATED,
+      rentBuyReq,
+      'Rent/Buy Property Request Created Successfully'
+    );
   }
 
-  async getAllRentBuyRequests(request: IRequest, response: Response): Promise<any> {
-    let  rentBuyReqs = await rentbuyReqService.getAllRequests();
+  async getAllRentBuyRequests(
+    request: IRequest,
+    response: Response
+  ): Promise<any> {
+    let rentBuyReqs = await rentbuyReqService.getAllRequests();
     //sending response
-    return Http.sendResponse(response, httpStatus.OK, rentBuyReqs, "Rent/Buy Property Requests Found");
+    return Http.sendResponse(
+      response,
+      httpStatus.OK,
+      rentBuyReqs,
+      'Rent/Buy Property Requests Found'
+    );
   }
 
   async getRentBuyRequest(request: IRequest, response: Response): Promise<any> {
     let rentBuyReq = await rentbuyReqService.getRequest(request.params.id);
     //sending response
-    return Http.sendResponse(response, httpStatus.OK, rentBuyReq, "Rent/Buy Property Request Found");
+    return Http.sendResponse(
+      response,
+      httpStatus.OK,
+      rentBuyReq,
+      'Rent/Buy Property Request Found'
+    );
   }
 
-  async updateRentBuyRequest(request: IRequest, response: Response): Promise<any> {
+  async updateRentBuyRequest(
+    request: IRequest,
+    response: Response
+  ): Promise<any> {
     let rentBuyReq;
     //validating json object
-    let body = validation.validateBody(request.body, rentbuyReqValidations.UPDATE);
+    let body = validation.validateBody(
+      request.body,
+      rentbuyReqValidations.UPDATE
+    );
     //update client
     rentBuyReq = await rentbuyReqService.updateRequest(request.params.id, body);
     //sending response
-    return Http.sendResponse(response, httpStatus.OK, rentBuyReq, "Rent/Buy Property Request Updated Successfully");
+    return Http.sendResponse(
+      response,
+      httpStatus.OK,
+      rentBuyReq,
+      'Rent/Buy Property Request Updated Successfully'
+    );
   }
 
-  async deleteRentBuyRequest(request: IRequest, response: Response): Promise<any> {
+  async deleteRentBuyRequest(
+    request: IRequest,
+    response: Response
+  ): Promise<any> {
     let rentBuyReq = await rentbuyReqService.deleteRequest(request.params.id);
     //sending response
-    return Http.sendResponse(response, httpStatus.OK, rentBuyReq, "Rent/Buy Property Request Deleted Successfully");
+    return Http.sendResponse(
+      response,
+      httpStatus.OK,
+      rentBuyReq,
+      'Rent/Buy Property Request Deleted Successfully'
+    );
   }
 }
 export default new RentBuyRequestController();
--- api/Request/RentBuyRequest/RentBuyRequestService.ts
+++ api/Request/RentBuyRequest/RentBuyRequestService.ts
@@ -3,10 +3,9 @@ import repository from './RentBuyRequestRepository';
 import NotFoundException from '../../../exception/NotFoundException';
 
 class RentBuyRequestService {
+  constructor() {}
 
-  constructor(){}
-
-  async find(conditions: any = {}): Promise<IRentBuyRequest[]>{
+  async getAllRequests(conditions: any = {}): Promise<IRentBuyRequest[]> {
     return await repository.find(conditions);
   }
 
@@ -14,44 +13,53 @@ class RentBuyRequestService {
     return await repository.create(body);
   }
 
-  async getRentBuyRequests(status: any, reqType: any): Promise<IRentBuyRequest[]> {
-    let rentBuyReqs = await repository.find({status: status, reqType: reqType});
-    if(rentBuyReqs.length === 0){
-      throw new NotFoundException("No Requests Found");
+  async getRentBuyRequests(
+    status: any,
+    reqType: any
+  ): Promise<IRentBuyRequest[]> {
+    let rentBuyReqs = await repository.find({
+      status: status,
+      reqType: reqType
+    });
+    if (rentBuyReqs.length === 0) {
+      throw new NotFoundException('No Requests Found');
     }
     return rentBuyReqs;
   }
 
   async getRequest(id: string): Promise<IRentBuyRequest> {
     let rentBuyReq = await repository.findById(id);
-    if(!rentBuyReq){
-      throw new NotFoundException("Request not found");
+    if (!rentBuyReq) {
+      throw new NotFoundException('Request not found');
     }
     return rentBuyReq;
   }
 
-  async updateRequest(id: string, body: IRentBuyRequest): Promise<IRentBuyRequest> {
+  async updateRequest(
+    id: string,
+    body: IRentBuyRequest
+  ): Promise<IRentBuyRequest> {
     let rentBuyReq = await repository.findByIdAndUpdate(id, body);
-    if(!rentBuyReq){
-      throw new NotFoundException("Request not found");
+    if (!rentBuyReq) {
+      throw new NotFoundException('Request not found');
     }
     return rentBuyReq;
   }
 
   async deleteRequest(id: string): Promise<boolean> {
     let isDeleted = await repository.softDeleteById(id);
-    if(!isDeleted){
-      throw new NotFoundException("Request not found");
+    if (!isDeleted) {
+      throw new NotFoundException('Request not found');
     }
     await repository.findByIdAndUpdate(id, { isDeleted: true });
     return isDeleted;
   }
 
-  async updateMany(conditions: any, updates: any): Promise<boolean>{
+  async updateMany(conditions: any, updates: any): Promise<boolean> {
     return await repository.flexibleUpdateMany(conditions, updates);
   }
 
-  async findOne(id: string): Promise<IRentBuyRequest>{
+  async findOne(id: string): Promise<IRentBuyRequest> {
     return repository.findOne(id);
   }
 }
